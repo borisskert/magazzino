0db077cec1d6bbbf0f5053f874f8dcceacf8661a
Boris Skert <boris.skert@byteagenten.de>
2024-04-11 09:34:25 +0200

    Create project with Spring Initializr

---
+++ src/test/java/de/borisskert/springboot/liquibaseexample/TestLiquibaseExampleApplication.java
@@ -0,0 +1,37 @@
+package de.borisskert.springboot.liquibaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestLiquibaseExampleApplication {
+
+	@Bean
+	@ServiceConnection
+	MariaDBContainer<?> mariaDbContainer() {
+		return new MariaDBContainer<>(DockerImageName.parse("mariadb:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+	}
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(LiquibaseExampleApplication::main).with(TestLiquibaseExampleApplication.class).run(args);
+	}
+
+}

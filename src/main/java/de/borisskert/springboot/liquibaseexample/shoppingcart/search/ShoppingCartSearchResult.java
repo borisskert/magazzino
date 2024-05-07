578ba837b7028dc4d923b74a55c0f31b844d9b0b
Boris Skert <boris.skert@byteagenten.de>
2024-05-07 17:44:55 +0200

    Cleanup search result DTO

--- src/main/java/de/borisskert/springboot/liquibaseexample/shoppingcart/search/ShoppingCartSearchResult.java
+++ src/main/java/de/borisskert/springboot/liquibaseexample/shoppingcart/search/ShoppingCartSearchResult.java
@@ -1,7 +1,6 @@
 package de.borisskert.springboot.liquibaseexample.shoppingcart.search;
 
 import de.borisskert.springboot.liquibaseexample.person.Person;
-import de.borisskert.springboot.liquibaseexample.product.Product;
 import de.borisskert.springboot.liquibaseexample.shoppingcart.persistence.ShoppingCart;
 import de.borisskert.springboot.liquibaseexample.shoppingcart.persistence.ShoppingCartItem;
 
@@ -9,72 +8,57 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
-public class ShoppingCartSearchResult {
+public record ShoppingCartSearchResult(
+        Long id,
+        Customer customer,
+        List<Item> items, Boolean checkedOut
+) {
 
-    public Long id;
-    public Customer customer;
-    public List<Item> items;
-    public Boolean checkedOut;
-
-    public static class Customer {
-        public Long id;
-        public String firstName;
-        public String lastName;
+    public record Customer(Long id, String firstName, String lastName) {
 
         public static Customer fromEntity(Person customer) {
-            Customer result = new Customer();
-            result.id = customer.getId();
-            result.firstName = customer.getFirstName();
-            result.lastName = customer.getLastName();
-
-            return result;
+            return new Customer(
+                    customer.getId(),
+                    customer.getFirstName(),
+                    customer.getLastName()
+            );
         }
     }
 
-    public static class ProductDto {
-        public UUID id;
-        public String name;
-        public String number;
-        public Double price;
-
-        public static ProductDto fromEntity(Product product) {
-            ProductDto result = new ProductDto();
-            result.id = product.getId();
-            result.name = product.getName();
-            result.number = product.getNumber();
-            result.price = product.getPrice();
-
-            return result;
+    public record Product(UUID id, String name, String number, Double price) {
+        
+        public static Product fromEntity(de.borisskert.springboot.liquibaseexample.product.Product product) {
+            return new Product(
+                    product.getId(),
+                    product.getName(),
+                    product.getNumber(),
+                    product.getPrice()
+            );
         }
     }
 
-    public static class Item {
-        public ProductDto product;
-        public Long quantity;
-
+    public record Item(Product product, Long quantity) {
+        
         public static Item fromEntity(ShoppingCartItem item) {
-            Item result = new Item();
-
-            result.quantity = item.getQuantity();
-            result.product = ProductDto.fromEntity(item.getProduct());
-
-            return result;
+            return new Item(
+                    Product.fromEntity(item.getProduct()),
+                    item.getQuantity()
+            );
         }
     }
 
     public static ShoppingCartSearchResult fromEntity(ShoppingCart cart) {
-        ShoppingCartSearchResult shoppingCartSearchResult = new ShoppingCartSearchResult();
-        shoppingCartSearchResult.id = cart.getId();
-
-        shoppingCartSearchResult.customer = ShoppingCartSearchResult.Customer.fromEntity(cart.getCustomer());
-        shoppingCartSearchResult.items = cart.getItems()
+        List<Item> items = cart.getItems()
                 .stream()
                 .map(Item::fromEntity)
-                .sorted(Comparator.comparing(a -> a.product.name))
+                .sorted(Comparator.comparing(item -> item.product.name))
                 .toList();
 
-        shoppingCartSearchResult.checkedOut = cart.isCheckedOut();
-
-        return shoppingCartSearchResult;
+        return new ShoppingCartSearchResult(
+                cart.getId(),
+                Customer.fromEntity(cart.getCustomer()),
+                items,
+                cart.isCheckedOut()
+        );
     }
 }

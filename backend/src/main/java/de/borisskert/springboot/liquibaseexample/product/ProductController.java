1b364c7ffa9689d512d5db1ccdf69aff6a55806b
Boris Skert <boris.skert@byteagenten.de>
2024-06-13 13:56:21 +0200

    Correct CORS config

--- backend/src/main/java/de/borisskert/springboot/liquibaseexample/product/ProductController.java
+++ backend/src/main/java/de/borisskert/springboot/liquibaseexample/product/ProductController.java
@@ -2,13 +2,10 @@ package de.borisskert.springboot.liquibaseexample.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
